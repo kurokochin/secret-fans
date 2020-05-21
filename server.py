ca1c0ba79f6465ffd5d2c77bd75a5a12421c667a
alyta <ar2519@ic.ac.uk>
2020-05-21 17:07:31 +0100

    Changed SQLalchemy to sqlite3

--- server.py
+++ server.py
@@ -2,10 +2,8 @@ from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Resource, Api
 from flask_restful import reqparse
-from json import dumps
-from sqlalchemy import create_engine
+from sqlite3 import connect
 
-db_connect = create_engine("sqlite:///secret_fans.db")
 app = Flask(__name__)
 api = Api(app)
 CORS(app, support_credentials=True)
@@ -20,19 +18,22 @@ class PostList(Resource):
         return {"id": raw_post[0], "recipient": raw_post[1], "content": raw_post[2]}
 
     def get(self):
-        conn = db_connect.connect() # Connect to database
-        query_result = conn.execute("SELECT rowid, recipient, content FROM post_tab;") # This line performs query and returns ResultObject
-        raw_posts = query_result.cursor.fetchall() # Convert ResultObject to list of list
+        connection = connect("secret_fans.db")
 
+        # This line performs query and returns ResultObject
+        query_result = connection.execute("SELECT rowid, recipient, content FROM post_tab;")
+        raw_posts = query_result.fetchall() # Convert ResultObject to list of tuple
         return {"posts": [self._convert_to_beautiful_post(raw_post) for raw_post in raw_posts]}
-    
-    def post(self):
-        args = self.parser.parse_args() # Parse the data
-        
-        conn = db_connect.connect() # Connect to database
-        query_result = conn.execute("INSERT INTO post_tab(`recipient`, `content`) VALUES ('{}', '{}');".format(args["recipient"], args["content"])) # This line performs query and returns ResultProxy
-        last_id = query_result.lastrowid
 
+    def post(self):
+        # Parse the input data
+        args = self.parser.parse_args()
+        # Connect to database
+        connection = connect("secret_fans.db")
+        # This line performs query and returns ResultProxy
+        query_result = connection.execute("INSERT INTO post_tab(`recipient`, `content`) VALUES (?, ?);", (args["recipient"], args["content"]))
+        connection.commit()
+        last_id = query_result.lastrowid
         return self._convert_to_beautiful_post([last_id, args["recipient"], args["content"]])
 
 
